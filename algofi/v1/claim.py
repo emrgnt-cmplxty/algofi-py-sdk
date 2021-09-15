3ea9c74946350db2e32ad9e3c1a925ebaa376fe0
Owen Colegrove <ocolegrove@gmail.com>
2021-09-15 10:52:38 -0400

    mods

--- algofi/v1/claim.py
+++ algofi/v1/claim.py
@@ -7,7 +7,7 @@ from algofi.config import assets, manager_id, escrow_hashes, storage_ids
 
 def prepare_claim_transactions(sender_addr, sender_key, params, amt, asset_name):
     (txn0, txn1, txn2, txn3) = get_init_txns(sender_addr, params)
-    txn4 = ApplicationNoOpTxn(sender_addr, params, manager_id, [b'claim'])
+    txn4 = ApplicationNoOpTxn(sender_addr, params, manager_id, [b'claim_mint'])
     txn5 = ApplicationNoOpTxn(sender_addr, params, storage_ids[asset_name], [], foreign_apps=[manager_id])
     txn6 = AssetTransferTxn(sender=sender_addr, sp=params, receiver=escrow_hashes[asset_name], amt=amt, index=assets[asset_name])
     return [txn0, txn1, txn2, txn3, txn4, txn5, txn6]
