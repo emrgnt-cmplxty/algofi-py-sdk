7538d76f1329efb008965e145b8e688054700293
Owen Colegrove <ocolegrove@gmail.com>
2021-09-15 09:40:48 -0400

    mods

--- algofi/v1/supply.py
+++ algofi/v1/supply.py
@@ -3,7 +3,6 @@ from os import name
 import algosdk
 from algosdk.future.transaction import ApplicationNoOpTxn, PaymentTxn, AssetTransferTxn
 from .prepend import get_init_txns
-from algofi.utils import package_all_tx
 from algofi.config import assets, manager_id, escrow_hashes, storage_ids
 
 def prepare_supply_transactions(sender_addr, sender_key, params, amt, asset_name):
@@ -11,5 +10,4 @@ def prepare_supply_transactions(sender_addr, sender_key, params, amt, asset_name
     txn4 = ApplicationNoOpTxn(sender_addr, params, manager_id, [b'mint'])
     txn5 = ApplicationNoOpTxn(sender_addr, params, storage_ids[asset_name], [], foreign_apps=[manager_id])
     txn6 = AssetTransferTxn(sender=sender_addr, sp=params, receiver=escrow_hashes[asset_name], amt=amt, index=assets[asset_name])
-    stxn_group = package_all_tx([txn0, txn1, txn2, txn3, txn4, txn5, txn6], [sender_key]*7, sign_last_wlogic=False)
-    return stxn_group
+    return [txn0, txn1, txn2, txn3, txn4, txn5, txn6]
